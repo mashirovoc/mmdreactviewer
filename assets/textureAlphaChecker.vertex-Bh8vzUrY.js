500bc20a1a011682812ec36c84fc2c0dfc524e6c
みずまんま <app.mizumizurice@gmail.com>
2025-02-01 22:51:45 +0900

    Updates

---
+++ assets/textureAlphaChecker.vertex-Bh8vzUrY.js
@@ -0,0 +1,8 @@
+import{y as r}from"./index-DbSqvsos.js";const e="textureAlphaCheckerVertexShader",t=`
+attribute uv: vec2f;varying vUv: vec2f;@vertex
+fn main(input: VertexInputs)->FragmentInputs {vertexOutputs.vUv=vertexInputs.uv;vertexOutputs.position=vec4f(
+(vertexInputs.uv % 1.0)*2.0-1.0,
+0.0,
+1.0
+);}
+`;r.ShadersStoreWGSL[e]=t;const v={name:e,shader:t};export{v as textureAlphaCheckerVertexShader};
