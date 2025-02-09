3a90da81b24c48e720ff3d5de294b2aa48919be0
みずまんま <app.mizumizurice@gmail.com>
2025-02-09 23:17:18 +0900

    Updates

--- assets/textureAlphaChecker.vertex-qFjWH1_q.js
+++ assets/textureAlphaChecker.vertex-qFjWH1_q.js
@@ -1,4 +1,4 @@
-import{Q as r}from"./index-50ZvmoW1.js";const e="textureAlphaCheckerVertexShader",t=`
+import{Q as r}from"./index-BoVMapfc.js";const e="textureAlphaCheckerVertexShader",t=`
 attribute uv: vec2f;varying vUv: vec2f;@vertex
 fn main(input: VertexInputs)->FragmentInputs {vertexOutputs.vUv=vertexInputs.uv;vertexOutputs.position=vec4f(
 (vertexInputs.uv % 1.0)*2.0-1.0,
