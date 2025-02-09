ff25d88dee857df2b9cd66121df6fdcf04bc45fb
みずまんま <app.mizumizurice@gmail.com>
2025-02-10 01:00:05 +0900

    Updates

--- assets/tgaTextureLoader-D-nP-_ph.js
+++
@@ -1,1 +0,0 @@
-import{k as r,l as n}from"./index-CjA6CRfh.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{f as _TGATextureLoader};
